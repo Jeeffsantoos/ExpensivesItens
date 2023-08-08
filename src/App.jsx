2817c80eef57b837955cdd709a880c7564377b29
Jefferson Alves <jeeffsantoos@outlook.com>
2023-08-07 22:57:38 -0300

    Curso React

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ExpenseItem from './components/ExpenseItem'
+
+function App() {
+return (
+    <div>
+      <h2>Lets get started!</h2>
+      <ExpenseItem></ExpenseItem>
+      <p>This is also visible!</p>
+    </div>
+  )
+}
+
+export default App
